4db4d1e0ef787d6ad5ae5b7698c6de6f520c671b
cral2744 <babacan4414@hotmail.com>
2015-08-30 02:32:28 +0200

    bot.user.js
    inciciler gelsin lan :D

--- bot.user.js
+++ bot.user.js
@@ -23,7 +23,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://inciagario.com/*
 // @version     3.61
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
